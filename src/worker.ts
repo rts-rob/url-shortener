3e90c2d30998d512f3faf32c1224502a0b87caa9
Rob Sutter <rsutter@cloudflare.com>
2023-05-25 21:07:32 -0400

    First version with KV + imported redirects.

--- src/worker.ts
+++ src/worker.ts
@@ -1,46 +1,32 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export interface Env {
+  URLS: KVNamespace;
+}
 
-import handleProxy from './proxy';
-import handleRedirect from './redirect';
-import apiRouter from './router';
-
-// Export a default object containing event handlers
 export default {
-	// The fetch handler is invoked when this worker receives a HTTP(S) request
-	// and should return a Response (optionally wrapped in a Promise)
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		// You'll find it helpful to parse the request.url string into a URL object. Learn more at https://developer.mozilla.org/en-US/docs/Web/API/URL
-		const url = new URL(request.url);
+  async fetch(request: Request, env: Env, _ctx: ExecutionContext): Promise<Response> {
+    // Build a standard JavaScript URL object from the request's URL
+    const url = new URL(request.url);
 
-		// You can get pretty far with simple logic like if/switch-statements
-		switch (url.pathname) {
-			case '/redirect':
-				return handleRedirect.fetch(request, env, ctx);
+    // Get the shortened path (key) from the url, removing the leading slash
+    const shortCode = url.pathname.split('/')[1];
 
-			case '/proxy':
-				return handleProxy.fetch(request, env, ctx);
-		}
+    // Redirect root url to Cloudflare docs (for now)
+    if (shortCode == "") {
+      Response.redirect("https://developers.cloudflare.com");
+    }
 
-		if (url.pathname.startsWith('/api/')) {
-			// You can also use more robust routing
-			return apiRouter.handle(request);
-		}
+    // Split off any querystring parameters
+    const params = url.searchParams;
 
-		return new Response(
-			`Try making requests to:
-      <ul>
-      <li><code><a href="/redirect?redirectUrl=https://example.com/">/redirect?redirectUrl=https://example.com/</a></code>,</li>
-      <li><code><a href="/proxy?modify&proxyUrl=https://example.com/">/proxy?modify&proxyUrl=https://example.com/</a></code>, or</li>
-      <li><code><a href="/api/todos">/api/todos</a></code></li>`,
-			{ headers: { 'Content-Type': 'text/html' } }
-		);
-	},
+    // Lookup the key in KV
+    const redirectUrl = await env.URLS.get(shortCode);
+
+    // If not found, return a 404
+    if (redirectUrl === null) {
+      return new Response("Value not found", { status: 404 });
+    }
+
+    // The Response class has static methods to create common Response objects as a convenience
+    return Response.redirect(redirectUrl + params);
+  },
 };
