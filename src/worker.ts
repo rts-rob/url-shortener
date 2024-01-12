1f7de42af054849315f47256c371eb0c034b7544
Rob Sutter <rsutter@cloudflare.com>
2024-01-12 14:22:07 +0100

    Add some OTel tracing

--- src/worker.ts
+++ src/worker.ts
@@ -1,18 +1,24 @@
 import html404 from './404.html';
 import { instrument, ResolveConfigFn } from '@microlabs/otel-cf-workers';
+import * as api from '@opentelemetry/api';
 
 export interface Env {
   BASELIME_API_KEY: string;
+  SERVICE_NAME: string
   URLS: KVNamespace;
 }
 
 const handler = {
   async fetch(request: Request, env: Env, _ctx: ExecutionContext): Promise<Response> {
+		const span = api.trace.getActiveSpan();
+		const { traceId } = span?.spanContext();
+
     // Build a standard JavaScript URL object from the request's URL
     const url = new URL(request.url);
 
     // Get the shortened path (key) from the url, removing the leading slash
     const shortCode = url.pathname.split('/')[1];
+		console.log(JSON.stringify({ shortCode, traceId }));
 
     // Redirect root url to Cloudflare docs (for now)
     if (shortCode == "") {
@@ -43,7 +49,7 @@ const config: ResolveConfigFn = (env: Env, _trigger) => {
 			url: 'https://otel.baselime.io/v1',
 			headers: { 'x-api-key': env.BASELIME_API_KEY },
 		},
-		service: { name: 'url-shortener' },
+		service: { name: env.SERVICE_NAME },
 	}
 };
 
