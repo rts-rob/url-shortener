e6e20c5d83113afea0c0dd8ee51e697d01fcc3c1
Rob Sutter <rsutter@cloudflare.com>
2024-09-19 10:43:11 +0200

    Added compatibility flag for skipping FL and enabled Workers Observability

--- src/worker.ts
+++ src/worker.ts
@@ -4,25 +4,25 @@ import * as api from '@opentelemetry/api';
 
 export interface Env {
   BASELIME_API_KEY: string;
-  SERVICE_NAME: string
+  SERVICE_NAME: string;
   URLS: KVNamespace;
 }
 
 const handler = {
   async fetch(request: Request, env: Env, _ctx: ExecutionContext): Promise<Response> {
-		const span = api.trace.getActiveSpan();
-		const { traceId } = span?.spanContext();
+    const span = api.trace.getActiveSpan();
+    const { traceId } = span?.spanContext();
 
     // Build a standard JavaScript URL object from the request's URL
     const url = new URL(request.url);
 
     // Get the shortened path (key) from the url, removing the leading slash
     const shortCode = url.pathname.split('/')[1];
-		console.log(JSON.stringify({ shortCode, traceId }));
+    console.log(JSON.stringify({ shortCode, traceId }));
 
     // Redirect root url to Cloudflare docs (for now)
-    if (shortCode == "") {
-      return Response.redirect("https://developers.cloudflare.com");
+    if (shortCode == '') {
+      return Response.redirect('https://developers.cloudflare.com');
     }
 
     // Split off any querystring parameters
@@ -30,6 +30,7 @@ const handler = {
 
     // Lookup the key in KV
     const redirectUrl = await env.URLS.get(shortCode);
+    // const { value } = await env.URLS.getWithMetadata(shortCode);
 
     // If not found, return a 404
     if (redirectUrl === null) {
@@ -44,13 +45,13 @@ const handler = {
 };
 
 const config: ResolveConfigFn = (env: Env, _trigger) => {
-	return {
-		exporter: {
-			url: 'https://otel.baselime.io/v1',
-			headers: { 'x-api-key': env.BASELIME_API_KEY },
-		},
-		service: { name: env.SERVICE_NAME },
-	}
+  return {
+    exporter: {
+      url: 'https://otel.baselime.io/v1',
+      headers: { 'x-api-key': env.BASELIME_API_KEY },
+    },
+    service: { name: env.SERVICE_NAME },
+  };
 };
 
 export default instrument(handler, config);
